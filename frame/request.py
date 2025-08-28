82c8b410adebe11d363249123cc4cda7bce65dbd
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2025-08-28 22:02:16 +0800

    refactor(frame): 重构 Requester 类
    - 添加 RequestConfig 类型注解
    - 优化类初始化，明确变量类型
    - 在请求循环中添加用户代理和超时设置
    - 调整日志输出格式
    - 提取 handle_response 方法处理响应逻辑
    -优化错误处理和类型检查

--- frame/request.py
+++ frame/request.py
@@ -7,18 +7,21 @@ from asyncio import sleep
 from httpx import Request, Response, AsyncClient, HTTPError
 
 from frame.bridge import Client
-from frame.config import Config
+from frame.config import Config, RequestConfig
 
 logger = getLogger(__name__)
 
 
 class Requester(object):
     def __init__(self, client: Client[Response | None, Request | None], config: Config):
-        self.config = config.REQUEST
-        self._channel = client
+        self.config: RequestConfig = config.REQUEST
+        self._channel: Client[Response | None, Request | None] = client
 
     async def loop(self):
-        async with AsyncClient() as client:
+        user_agent: dict[str, str] = self.config.DEFAULT_REQUEST_HEADERS
+        user_agent['user-agent'] = self.config.USER_AGENT
+
+        async with AsyncClient(headers=user_agent, timeout=self.config.DOWNLOAD_DELAY) as client:
             while True:
                 request: Request | None = await self._channel.get()
 
@@ -26,17 +29,12 @@ class Requester(object):
                     logger.info('receive close signal, stopping program')
                     break
 
-                logger.debug(f'requesting {request.url}...')
+                logger.debug(f'requesting {request.url} ...')
                 response: Response | None = await self.handle_request(request, client)
+                await  self.handle_response(response, request.url)
 
-                if isinstance(response, Response):
-                    logger.debug(f'{request.url} succeeded')
-                    self._channel.put(response)
-                else:
-                    logger.error(f'{request.url} failed to the maximum number of retries')
-
-                logger.debug(f'sleeping for {self.config.INTERVAL} seconds...')
-                await sleep(self.config.INTERVAL)
+                logger.debug(f'sleeping for {self.config.DOWNLOAD_DELAY} seconds...')
+                await sleep(self.config.DOWNLOAD_DELAY)
 
     async def handle_request(self, request: Request, client: AsyncClient) -> Response | None:
         for i in range(self.config.MAX_RETRY):
@@ -49,6 +47,16 @@ class Requester(object):
                 return  response
 
         return None
+
+    async def handle_response(self, response: Response | None, url: str):
+        if response is None:
+            logger.error(f'{url} failed to the maximum number of retries')
+        elif isinstance(response, Response):
+            logger.debug(f'{url} succeeded')
+            await self._channel.put(response)
+        else:
+            raise TypeError(f'response must be a Response or None, {type(response)} given')
+
 
 
 if __name__ == '__main__':
