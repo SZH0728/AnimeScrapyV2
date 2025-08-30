1896e6d8b29e58409fbfb5b62dc1ed82ab54d2ff
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2025-08-30 15:15:19 +0800

    refactor(summarize): 重构数据收集模块并添加文档注释
    - 为 Collect 和 WebPriority 类添加详细的文档注释，说明类和方法的功能
    - 优化 Collect 类的 main 和 cycle 方法，提高代码可读性
    - 在创建和更新评分信息时增加日志记录，便于调试
    - 在 WebPriority 类中添加获取当前网站 ID 和名称的方法
    - 优化 WebPriority 类的 next 和 previous 方法，明确返回值含义

--- summarize/collect.py
+++ summarize/collect.py
@@ -14,10 +14,28 @@ logger = getLogger(__name__)
 
 
 class Collect(object):
+    """
+    @brief 数据收集和处理类
+
+    该类负责从缓存中收集动漫数据，处理并存储到详细信息表和评分表中
+    """
     def __init__(self):
+        """
+        @brief 初始化Collect对象
+
+        创建WebPriority对象用于处理不同网站的数据优先级
+        """
         self._web_priority: WebPriority = WebPriority()
 
     def main(self) -> list[tuple[str, str]]:
+        """
+        @brief 主循环函数，收集所有网站的数据
+
+        通过循环遍历所有网站，收集数据并处理
+
+        @return 返回一个元组列表，每个元组包含detail ID和图片URL
+        @retval list[tuple[str, str]] 包含(detail_id, picture_url)的元组列表
+        """
         logger.info('Start to collect data')
         pictures: list[tuple[str, str]] = []
         with SessionFactory() as session:
@@ -32,6 +50,16 @@ class Collect(object):
         return pictures
 
     def cycle(self, web_id: int, session: Session) -> list[tuple[str, str]]:
+        """
+        @brief 处理单个网站的数据收集周期
+
+        查询指定网站的缓存数据，检查是否已存在对应的详细信息，如果不存在则创建，否则更新
+
+        @param web_id 网站ID
+        @param session 数据库会话对象
+        @return 返回图片URL列表，格式为(detail_id, picture_url)元组
+        @retval list[tuple[str, str]] 包含(detail_id, picture_url)的元组列表
+        """
         logger.debug(f'Start to collect data from {self._web_priority.current_web_name}')
         caches: list[Cache] = session.query(Cache).filter(Cache.web == web_id).all()  # type: ignore
 
@@ -49,6 +77,8 @@ class Collect(object):
                 logger.debug(f'detail {cache.name} exists, update detail')
                 self.update_score(cache, detail, session)
 
+            #TODO: 当构建项目时，记得启用删除缓存数据
+
             # session.delete(cache)
             # session.commit()
 
@@ -56,6 +86,15 @@ class Collect(object):
 
     @staticmethod
     def update_score(cache: Cache, detail: Detail, session: Session):
+        """
+        @brief 更新已存在的动漫条目的评分信息
+
+        根据缓存中的数据更新评分表中的详细评分和总评分
+
+        @param cache 缓存数据对象
+        @param detail 详细信息对象
+        @param session 数据库会话对象
+        """
         score: Score = session.query(Score).filter(Score.detailId == detail.id).first()  # type: ignore
 
         score.detailScore[cache.webId] = [float(cache.score), cache.vote]
@@ -70,6 +109,14 @@ class Collect(object):
         session.commit()
 
     def create_score(self, cache: Cache, session: Session) -> tuple[Detail, Score]:
+        """
+        @brief 为新的动漫条目创建详细信息和评分记录
+
+        @param cache 缓存数据对象
+        @param session 数据库会话对象
+        @return 返回新创建的详细信息对象和评分对象
+        @retval tuple[Detail, Score] 包含详细信息对象和评分对象的元组
+        """
         detail, score = self.split_cache_to_detail_and_score(cache, session)
 
         session.add(score)
@@ -80,6 +127,14 @@ class Collect(object):
 
     @staticmethod
     def split_cache_to_detail_and_score(cache: Cache, session: Session) -> tuple[Detail, Score]:
+        """
+        @brief 将缓存数据分割为详细信息和评分两个对象
+
+        @param cache 缓存数据对象
+        @param session 数据库会话对象
+        @return 返回详细信息对象和评分对象
+        @retval tuple[Detail, Score] 包含详细信息对象和评分对象的元组
+        """
         # 创建Detail对象并填充相应字段
         detail = Detail(
             name=cache.name,
--- summarize/priority.py
+++ summarize/priority.py
@@ -9,7 +9,17 @@ logger = getLogger(__name__)
 
 
 class WebPriority(object):
+    """
+    @brief 网站优先级管理类
+
+    该类用于管理和遍历按照优先级排序的网站列表，支持向前和向后遍历网站列表
+    """
     def __init__(self):
+        """
+        @brief 初始化WebPriority对象
+
+        从数据库中查询所有网站，并按优先级升序排列存储在内部列表中
+        """
         logger.debug('Initialize WebPriority, selecting webs...')
         with SessionFactory() as session:
             self._webs: list[Web] = session.query(Web).order_by(Web.priority.asc()).all()
@@ -19,13 +29,33 @@ class WebPriority(object):
 
     @property
     def current_web_id(self) -> int:
+        """
+        @brief 获取当前网站的ID
+
+        @return 当前网站的ID
+        @retval int 当前网站的ID值
+        """
         return self._webs[self._current].id
 
     @property
     def current_web_name(self):
+        """
+        @brief 获取当前网站的名称
+
+        @return 当前网站的名称
+        @retval str 当前网站的名称
+        """
         return self._webs[self._current].name
 
     def next(self) -> bool:
+        """
+        @brief 移动到下一个网站
+
+        将当前索引向前移动一位，如果已经到达列表末尾则不移动并返回False
+
+        @return 是否成功移动到下一个网站
+        @retval bool True表示成功移动，False表示已到达列表末尾
+        """
         self._current += 1
 
         if self._current >= len(self._webs):
@@ -35,6 +65,14 @@ class WebPriority(object):
         return True
 
     def previous(self) -> bool:
+        """
+        @brief 移动到上一个网站
+
+        将当前索引向后移动一位，如果已经到达列表开头则不移动并返回False
+
+        @return 是否成功移动到上一个网站
+        @retval bool True表示成功移动，False表示已到达列表开头
+        """
         self._current -= 1
 
         if self._current < 0:
